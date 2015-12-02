f1d6e5b1bd3a980427ef8bce0994014a45b0ed20
Thanh Chung <thanhchungbtc@gmail.com>
2015-12-02 16:49:57 +0700

    Edit dijsktra

--- src/SearchAlgorithm/Model/DijkstraPathfinderEngine.java
+++ src/SearchAlgorithm/Model/DijkstraPathfinderEngine.java
@@ -14,25 +14,30 @@ public class DijkstraPathfinderEngine extends AbstractPathfinderEngine {
       SearchResult searchResult = new SearchResult();
 
       int n = graph.getVertices().size();
-      int[] d = new int[n];
+      int[] d = new int[n]; for (int i = 0; i < n; i++) d[i] = graph.Max_INFINITY;
       boolean[] free = new boolean[n]; for (int i = 0; i < n; i++) free[i] = true;
       int[] trace = new int[n];
       d[fromVertex.id] = 0;
 
       int u, v;
       while (true) {
-         u = 0;
+         // find the vertex u has d[u] min
+         u = -1;
          int min = graph.Max_INFINITY;
          for (int i = 0; i < n; i++) {
             if (free[i] && d[i] < min) {
+               min = d[i];
                u = i;
             }
          }
-         if (u == 0 || u == toVertex.id) break;
+         // u = -1 means there's no vertex to optimize anymore, algorithm should stop
+         if (u == -1 || u == toVertex.id) break;
+         // mark u as optimized
          free[u] = false;
          for (v = 0; v < n; v++) {
             int cost = graph.getCostBetween2Vertices(graph.getVertexByID(u), graph.getVertexByID(v));
             if (free[v] && d[v] > d[u] + cost) {
+
                d[v] = d[u] + cost;
                trace[v] = u;
             }
